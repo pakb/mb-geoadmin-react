156c67372826825ed4d2addceb73388e26d30d1d
Pascal Barth <pak.barth@gmail.com>
2019-04-26 10:45:47 +0200

    Adaptations for colloquium

--- src/deckgl-map.component.js
+++ src/deckgl-map.component.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { StaticMap } from "react-map-gl";
 import DeckGL, { HexagonLayer } from "deck.gl";
-import cloneDeep from "lodash/cloneDeep";
 import * as d3 from "d3"
 
 import "mapbox-gl/dist/mapbox-gl.css"
 
-const MAPBOX_STYLE_URL =
+const MAPBOX_STYLE_URL_WITH_BUILDINGS =
   "https://tileserver.int.bgdi.ch/gl-styles/ch.swisstopo.leichte-basiskarte.vt/v006_3d/style.json";
 
   const DATA_URL =
@@ -26,27 +25,22 @@ export default class DeckGLMap extends Component {
     super(props);
 
     this.state = {
+      showBuildings: false,
+      showHeatmap: false,
       baseStyleJson: undefined,
-      styleWithVisibleLayers: undefined,
-      styleJsonConfig: {
-        lastLayerIdBeforeLabels: "boundary_"
-      },
+      styleJsonWithBuildings: undefined,
       viewport: {},
-      visibleLayers: props.visibleLayers,
       layerData: undefined,
       elevationScale: 5,
-      radius: 10,
+      radius: 1000,
       coverage: 0.9,
       maxElevation: 1000,
       lastZoomLevel: 0
     };
   }
-  componentWillReceiveProps(nextProps) {
-    this.setState({ visibleLayers: nextProps.visibleLayers });
-  }
 
   componentDidMount = () => {
-    fetch(MAPBOX_STYLE_URL)
+    fetch(MAPBOX_STYLE_URL_WITH_BUILDINGS)
       .then(res => res.json())
       .then(data => {
         this.setState({
@@ -69,94 +63,15 @@ export default class DeckGLMap extends Component {
     });
   };
 
-  buildStyleWithVisibleLayers = styleJson => {
-    if (styleJson) {
-      let styleWithVisibleLayers = cloneDeep(styleJson);
-      let indexWhereToInsert = styleWithVisibleLayers.layers.length - 1;
-      styleWithVisibleLayers.layers.forEach((layer, index) => {
-        if (layer.id === this.state.styleJsonConfig.lastLayerIdBeforeLabels) {
-          indexWhereToInsert = index;
-        }
-      });
-      this.state.visibleLayers.forEach(layer => {
-        if (!layer.show) {
-          return;
-        }
-        if (layer.type === "wmts") {
-          let timestamp = "current";
-          if (layer.timestamps && layer.timestamps.length > 0) {
-            if (layer.timeEnabled || layer.timestamps.length === 1) {
-              timestamp = layer.timestamps[0];
-            }
-          }
-          styleWithVisibleLayers.sources[layer.id] = {
-            type: "raster",
-            tiles: [
-              "https://wmts5.geo.admin.ch/1.0.0/" +
-                layer.id +
-                "/default/" +
-                timestamp +
-                "/3857/{z}/{x}/{y}." +
-                layer.format
-            ],
-            tileSize: 256
-          };
-        } else if (layer.type === "wms") {
-          styleWithVisibleLayers.sources[layer.id] = {
-            type: "raster",
-            tiles: [
-              "https://wms4.geo.admin.ch/?service=WMS&version=1.3.0&request=GetMap&format=image/png&transparent=true&layers=" +
-                layer.id +
-                "&lang=en&width=256&height=256&crs=EPSG:3857&style=&bbox={bbox-epsg-3857}"
-            ],
-            tileSize: 256
-          };
-        } else if (layer.type === "geojson") {
-          styleWithVisibleLayers.sources[layer.id] = {
-            type: "geojson",
-            data: layer.geojsonUrl
-          };
-          console.log("geojsonUrl", layer.geojsonUrl, layer);
-        } else {
-          console.log(
-            "layer " + layer.id + " has an unsupported type " + layer.type
-          );
-        }
-        if (layer.type === "wmts" || layer.type === "wms") {
-          styleWithVisibleLayers.layers.splice(indexWhereToInsert, 0, {
-            id: "layer_" + layer.id,
-            type: "raster",
-            source: layer.id,
-            paint: {
-              "raster-opacity": layer.opacity
-            }
-          });
-        } else if (layer.type === "geojson") {
-          styleWithVisibleLayers.layers.splice(indexWhereToInsert, 0, {
-            id: "layer_" + layer.id,
-            type: "fill",
-            source: layer.id,
-            paint: {
-              "fill-color": "rgb(0, 255, 0)",
-              "fill-opacity": layer.opacity
-            }
-          });
-        }
-      });
-      return styleWithVisibleLayers;
-    }
-  };
-
-  onClick = (info, allInfos, event) => {
-    const lonlat = this.staticMap.getMap().unproject([event.offsetX, event.offsetY]);
-    console.log('click at', lonlat)
+  onClick = (event) => {
+    console.log('click at', event.lngLat)
   };
 
   getLayers = () => {
-    if (this.state.layerData) {
-      const {radius = 10, upperPercentile = 100, coverage = 0.9} = this.state;
-      console.log('radius', radius);
+    if (this.state.showHeatmap && this.state.layerData) {
+      const {radius = 1000, upperPercentile = 100, coverage = 1} = this.state;
       const data = this.state.layerData;
+      console.log('radius :', radius)
       return [new HexagonLayer({
         id: 'heatmap',
         colorRange,
@@ -166,10 +81,7 @@ export default class DeckGLMap extends Component {
         elevationScale: this.state.elevationScale,
         extruded: true,
         getPosition: d => d,
-        //lightSettings: LIGHT_SETTINGS,
-        onHover: this.props.onHover,
-        opacity: 0.1,
-        pickable: Boolean(this.props.onHover),
+        opacity: 0.3,
         radius,
         upperPercentile
       })]
@@ -181,42 +93,40 @@ export default class DeckGLMap extends Component {
   _onMapLoad = () => {
     const mapbox = this.staticMap.getMap();
     mapbox.setLight({color: "#fff", intensity: 0.8, position: [1.15, 135, 45]});
-    this.setState({lastZoomLevel: mapbox.getZoom()});
-    mapbox.on('zoom', () => {
-      const zoomLevel = mapbox.getZoom();
-      this.setState({lastZoomLevel: zoomLevel});
-      setTimeout(() => {
-        if (this.state.lastZoomLevel === zoomLevel) {
-          const newRadius = (zoomLevel * 1000.0) / Math.pow(2, zoomLevel - 5);
-          this.setState({
-            radius: newRadius,
-            maxElevation: Math.min(newRadius * (33.0 / zoomLevel), 20000),
-            coverage: zoomLevel > 12 ? 0.5 : 0.9
-          })
-        }
-      }, 400)
-    })
   };
 
   render() {
     const { viewState, controller = true } = this.props;
     if (this.state.baseStyleJson) {
+      let heatmapControls = null;
+      if (this.state.showHeatmap) {
+        heatmapControls = (<>
+          <br/>
+          <div className="heatmap-controls">
+            Radius ({this.state.radius}) : <input type="range" min="10" max="2000" step="10" value={this.state.radius} onChange={(e) => { this.setState({ radius: e.target.value }) }}></input><br/>
+            Max elevation ({this.state.maxElevation}) : <input type="range" min="10" max="3000" step="10" value={this.state.maxElevation} onChange={(e) => { this.setState({ maxElevation: e.target.value }) }}></input><br/>
+            Coverage ({this.state.coverage}) : <input type="range" min="0.1" max="1" step="0.1" value={this.state.coverage} onChange={(e) => { this.setState({ coverage: e.target.value }) }}></input><br/>
+          </div>
+        </>);
+      }
       return (
         <div>
+          <div className="colloquium-menu">
+            With UK car accidents : <input type="checkbox" value={this.state.showHeatmap} onClick={(e) => this.setState({ showHeatmap: !this.state.showHeatmap })}></input>
+            { heatmapControls }
+          </div>
           <DeckGL
             initialViewState={this.state.viewport}
             viewState={viewState}
             controller={controller}
-            onLayerClick={ (info, allInfos, event) => this.onClick(info, allInfos, event) }
+            onClick={ (info, allInfos, event) => this.onClick(info, allInfos, event) }
             layers={this.getLayers()}
           >
             <StaticMap
               ref={staticMap => this.staticMap = staticMap}
               reuseMaps
-              mapStyle={this.buildStyleWithVisibleLayers(
-                this.state.baseStyleJson
-              )}
-              preventStyleDiffing={false}
+              mapStyle={ this.state.baseStyleJson }
+              preventStyleDiffing={true}
               onLoad={this._onMapLoad}
             />
           </DeckGL>
--- src/index.js
+++ src/index.js
@@ -2,105 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import DeckGLMap from "./deckgl-map.component";
-import Menu from "./menu/menu.component";
-
-import CatalogueService from "./services/catalogue.service";
-import LayerService from "./services/layer.service";
 
 import "./styles.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      catalogue: null,
-      visibleLayers: []
-    };
-  }
-
-  componentDidMount = () => {
-    Promise.all([
-      CatalogueService.getCatalogue(),
-      LayerService.getLayers()
-    ]).then(([catalogue, layers]) => {
-      this.setState({
-        catalogue: catalogue,
-        visibleLayers: LayerService.getVisibleLayers()
-      });
-    });
-  };
-
-  handleOnCatalogueLayerClick = (event, catalogueLayer) => {
-    const layer = LayerService.getLayerForId(catalogueLayer.layerBodId);
-    if (!layer) {
-      console.log(
-        "Error, no layer found for bod ID " + catalogueLayer.layerBodId
-      );
-      return;
-    }
-    catalogueLayer.show = !catalogueLayer.show;
-    if (catalogueLayer.show) {
-      LayerService.addVisibleLayer(catalogueLayer.layerBodId);
-    } else {
-      LayerService.hideVisibleLayer(catalogueLayer.layerBodId);
-    }
-    this.setState({
-      visibleLayers: LayerService.getVisibleLayers()
-    });
-  };
-
-  handleLayerOpacityChanged = (layer, newOpacity) => {
-    layer.opacity = newOpacity;
-    this.forceUpdate();
-  };
-
-  handleRemoveLayerFromHighlight = (layer) => {
-    LayerService.removeLayerIfVisible(layer);
-    const catalogueItem = CatalogueService.getCatalogueItemForLayerId(layer.id);
-    catalogueItem.show = false;
-    this.setState({
-      visibleLayers: LayerService.getVisibleLayers()
-    })
-  }
-
-  handleRaiseVisibleLayerOrder = (layer) => {
-    LayerService.raiseVisibleLayerOrder(layer);
-    this.setState({
-      visibleLayers: LayerService.getVisibleLayers()
-    })
-  }
-
-  handleLowerVisibleLayerOrder = (layer) => {
-    LayerService.lowerVisibleLayerOrder(layer);
-    this.setState({
-      visibleLayers: LayerService.getVisibleLayers()
-    })
-  }
-
-  handleLayerToggleShow = (layer) => {
-    const visibleLayer = LayerService.getVisibleLayer(layer.id);
-    visibleLayer.show = !visibleLayer.show;
-    const catalogueItem = CatalogueService.getCatalogueItemForLayerId(layer.id);
-    catalogueItem.show = visibleLayer.show;
-    this.setState({
-      visibleLayers: LayerService.getVisibleLayers()
-    })
-  }
 
   render = () => {
     return (
       <div className="App">
-        <Menu
-          catalogue={this.state.catalogue}
-          visibleCatalogueItems={this.state.visibleCatalogueItems}
-          handleOnCatalogueLayerClick={this.handleOnCatalogueLayerClick}
-          handleLayerOpacityChanged={this.handleLayerOpacityChanged}
-          handleRemoveLayerFromHighlight={this.handleRemoveLayerFromHighlight}
-          handleRaiseVisibleLayerOrder={this.handleRaiseVisibleLayerOrder}
-          handleLowerVisibleLayerOrder={this.handleLowerVisibleLayerOrder}
-          handleLayerToggleShow={this.handleLayerToggleShow}
-        />
-        <DeckGLMap visibleLayers={this.state.visibleLayers} />
+        <DeckGLMap />
       </div>
     );
   };
